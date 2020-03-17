d2fcb0e0e370011fd5352049a813f895c05e1de8
Sameer Vasantgadkar <sameerdg0379@gmail.com>
2020-03-17 20:50:48 +0530

    Made changes to auto pick latest date for rendering map and set max bounds to avoid dragging outside of scope.

--- covid19-deaths-animation/covid_deaths_bubble_map.R
+++ covid19-deaths-animation/covid_deaths_bubble_map.R
@@ -9,11 +9,11 @@ library(data.table)
 library(data.table)
 library(stringr)
 
-selected_date <- "3/13/20"
-
 confirmed_data <- read_csv("../data/time_series_19-covid-Confirmed.csv")
 confirmed_data <- confirmed_data %>% gather(day, cases, '1/22/20':ncol(confirmed_data))
 confirmed_data <- confirmed_data %>% mutate(day = as.Date(day, format = "%m/%d/%y"))
+
+selected_date <- max(confirmed_data$day)
 
 deaths_data <- read_csv("../data/time_series_19-covid-Deaths.csv")
 deaths_data <- deaths_data %>% gather(day, deaths, '1/22/20':ncol(deaths_data))
@@ -45,14 +45,15 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   output$map <- renderLeaflet({
-    leaflet(merged_data, options = leafletOptions()) %>%
+    leaflet(merged_data, options = leafletOptions(minZoom = 3, maxZoom = 3, worldCopyJump = FALSE, zoomControl = FALSE, maxBoundsViscocity = 1.0)) %>%
+      setMaxBounds(lng1 = -180, lat1 = -90, lng2 = 180, lat2 = 90) %>%
       setView(lng = 79.08, lat = 21.14, zoom = 3) %>% #center at India and with initial zoom to 3
-      addProviderTiles("CartoDB.Positron") %>%
+      addProviderTiles("CartoDB.Positron", options = providerTileOptions(noWrap = TRUE)) %>%
       addCircleMarkers(merged_data$Long, merged_data$Lat,
                        fillColor = "#FF0000", fillOpacity = 0.5, color="white", radius= merged_data$cases * 0.001 + 10, stroke=FALSE,
                        label = map_text,
                        labelOptions = labelOptions( style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "13px", direction = "auto")
-      )  
+      )
   })
 }
 
