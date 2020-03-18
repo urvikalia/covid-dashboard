f389a9ea9893bb054eac7b64840f5aac88644aa6
Sameer Vasantgadkar <sameerdg0379@gmail.com>
2020-03-18 11:22:52 +0530

    Made changes to show loading indicator while fetching data

--- covid19-deaths-animation/covid_deaths_bubble_map.R
+++ covid19-deaths-animation/covid_deaths_bubble_map.R
@@ -1,62 +1,31 @@
 library(shiny)
 library(leaflet)
 library(tidyverse)
-library(gganimate)
-library(ggplot2)
-library(tidyverse)
-library(gifski)
-library(data.table)
 library(data.table)
 library(stringr)
+library(waiter)
 
 setwd("../") #updated working dir to fetch data at parent level
-if(!dir.exists("./Covid-19")){
-  system("git clone https://github.com/CSSEGISandData/COVID-19.git")
-} else {
-  system("cd Covid-19/")
-  system("git pull origin master")
-}
-
-data_root_path <- "./COVID-19/csse_covid_19_data/csse_covid_19_time_series"
-
-confirmed_data <- read_csv(paste0(data_root_path, "/time_series_19-covid-Confirmed.csv"))
-confirmed_data <- confirmed_data %>% gather(day, cases, '1/22/20':ncol(confirmed_data))
-confirmed_data <- confirmed_data %>% mutate(day = as.Date(day, format = "%m/%d/%y"))
-
-selected_date <- max(confirmed_data$day)
-
-deaths_data <- read_csv(paste0(data_root_path, "/time_series_19-covid-Deaths.csv"))
-deaths_data <- deaths_data %>% gather(day, deaths, '1/22/20':ncol(deaths_data))
-deaths_data <- deaths_data %>% mutate(day = as.Date(day, format = "%m/%d/%y"))
-
-recovered_data <- read_csv(paste0(data_root_path, "/time_series_19-covid-Recovered.csv"))
-recovered_data <- recovered_data %>% gather(day, recovered, '1/22/20':ncol(recovered_data))
-recovered_data <- recovered_data %>% mutate(day = as.Date(day, format = "%m/%d/%y"))
-
-merged_data <- merge(confirmed_data, deaths_data, by=c('Province/State', 'Country/Region', 'Lat', 'Long', 'day'))
-merged_data <- merge(merged_data, recovered_data, by=c('Province/State', 'Country/Region', 'Lat', 'Long', 'day'))
-
-merged_data <- merged_data %>% filter(cases > 10, day == as.Date(selected_date, format = "%m/%d/%y"))
-# merged_data <- merged_data %>% filter(str_detect(`Country/Region`, "China"))
-
-map_text <- paste(
-  "Cases : ", merged_data$cases, "<br/>",
-  "Recovered : ", merged_data$recovered , "<br/>",
-  "Deaths : ", merged_data$deaths , "<br/>",
-  "Active : ", merged_data$cases - merged_data$recovered - merged_data$deaths, "<br/>",
-  "Province/State : ", merged_data$`Province/State`, "<br/>",
-  "Country : ", merged_data$`Country/Region`, sep="") %>%
-  lapply(htmltools::HTML)
 
 ui <- fluidPage(
   fluidRow(
+    use_waiter(),
+    waiter_show_on_load(),
     leafletOutput("map", height=1000)
   )
 )
 
 server <- function(input, output) {
+  
   output$map <- renderLeaflet({
+    fetchLatestData()
 
+    retValue <- loadData()
+    merged_data <- retValue[[1]]
+    map_text <- retValue[[2]]
+    
+    hide_waiter()
+    
     leaflet(merged_data, options = leafletOptions(minZoom = 3, maxZoom = 3, worldCopyJump = FALSE, zoomControl = FALSE, maxBoundsViscocity = 1.0)) %>%
       setMaxBounds(lng1 = -180, lat1 = -90, lng2 = 180, lat2 = 90) %>%
       setView(lng = 79.08, lat = 21.14, zoom = 3) %>% #center at India and with initial zoom to 3
@@ -67,8 +36,51 @@ server <- function(input, output) {
                        labelOptions = labelOptions( style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "13px", direction = "auto")
       )
   })
+  
+  fetchLatestData <- function() {
+    if(!dir.exists("./Covid-19")){
+      system("git clone https://github.com/CSSEGISandData/COVID-19.git")
+    } else {
+      system("cd Covid-19/")
+      system("git pull origin master")
+    }
+  }
+  
+  loadData <- function() {
+    data_root_path <- "./COVID-19/csse_covid_19_data/csse_covid_19_time_series"
+    
+    confirmed_data <- read_csv(paste0(data_root_path, "/time_series_19-covid-Confirmed.csv"))
+    confirmed_data <- confirmed_data %>% gather(day, cases, '1/22/20':ncol(confirmed_data))
+    confirmed_data <- confirmed_data %>% mutate(day = as.Date(day, format = "%m/%d/%y"))
+    
+    selected_date <- max(confirmed_data$day)
+    
+    deaths_data <- read_csv(paste0(data_root_path, "/time_series_19-covid-Deaths.csv"))
+    deaths_data <- deaths_data %>% gather(day, deaths, '1/22/20':ncol(deaths_data))
+    deaths_data <- deaths_data %>% mutate(day = as.Date(day, format = "%m/%d/%y"))
+    
+    recovered_data <- read_csv(paste0(data_root_path, "/time_series_19-covid-Recovered.csv"))
+    recovered_data <- recovered_data %>% gather(day, recovered, '1/22/20':ncol(recovered_data))
+    recovered_data <- recovered_data %>% mutate(day = as.Date(day, format = "%m/%d/%y"))
+    
+    merged_data <- merge(confirmed_data, deaths_data, by=c('Province/State', 'Country/Region', 'Lat', 'Long', 'day'))
+    merged_data <- merge(merged_data, recovered_data, by=c('Province/State', 'Country/Region', 'Lat', 'Long', 'day'))
+    
+    merged_data <- merged_data %>% filter(cases > 10, day == as.Date(selected_date, format = "%m/%d/%y"))
+    # merged_data <- merged_data %>% filter(str_detect(`Country/Region`, "China"))
+    
+    map_text <- paste(
+      "Cases : ", merged_data$cases, "<br/>",
+      "Recovered : ", merged_data$recovered , "<br/>",
+      "Deaths : ", merged_data$deaths , "<br/>",
+      "Active : ", merged_data$cases - merged_data$recovered - merged_data$deaths, "<br/>",
+      "Province/State : ", merged_data$`Province/State`, "<br/>",
+      "Country : ", merged_data$`Country/Region`, sep="") %>%
+      lapply(htmltools::HTML)
+    
+    return(list(merged_data, map_text))
+  }
 }
-
 
 # Run the application
 shinyApp(ui = ui, server = server)
